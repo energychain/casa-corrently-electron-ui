f3bbc3ce5920b9902372cfb46435146c7ace9916
Thorsten Zoerner <me@thorsten-zoerner.com>
2020-09-03 01:44:21 +0200

    Initial Commit

--- main.js
+++ main.js
@@ -5,9 +5,40 @@ const electron = require("electron"),
 let mainWindow;
 
 function createWindow() {
+  const CasaCorrently = require("casa-corrently");
+  const fs = require("fs");
+
+  const fileExists = async path => !!(await fs.promises.stat(path).catch(e => false));
+
+  const boot = async function() {
+    let config = {};
+    if(typeof process.env.PORT !== 'undefined') {
+      port = process.env.PORT;
+    }
+    // UUID Persistence
+    if((process.argv.length == 3)&&(await fileExists(process.argv[2]))) {
+      config = JSON.parse(fs.readFileSync(process.argv[2]));
+    } else
+    if(await fileExists("./config.json")) {
+      config = JSON.parse(fs.readFileSync("./config.json"));
+    } else
+    if(await fileExists("./sample_config.json")) {
+      config = JSON.parse(fs.readFileSync("./sample_config.json"));
+    }
+    if(typeof config.uuid == 'undefined') {
+      config.uuid = Math.random(); // Due to node js incompatibility with nanoid or uuid this is a bad fix
+      config.uuid = (""+config.uuid).substring(2) + (Math.random());
+    }
+    config.staticFiles = './resources/app/express-app/public';
+    const main = await CasaCorrently();
+    await main.server(config);
+  };
+
+  boot();
+
   mainWindow = new BrowserWindow({
-    autoHideMenuBar: true,
-    width: 1280,
+    autoHideMenuBar: false,
+    width: 1300,
     height: 720,
     useContentSize: true,
     webPreferences: {
